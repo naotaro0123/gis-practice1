2dc80c932cd9a07a4183fd26ca02169affcf7e3e
naotaro0123 <n.ikewada@gmail.com>
2025-01-03 16:20:26 +0900

    fix: add markers

--- src/marker-map.ts
+++ src/marker-map.ts
@@ -5,4 +5,22 @@ export const setupMarkerMap = (map: L.Map) => {
   const marker = L.marker([33, 129.5]);
   marker.bindPopup("平面直角座標1系原点");
   map.addLayer(marker);
+
+  L.marker([33, 131.0]).bindPopup("平面直角座標2系原点").addTo(map);
+  L.marker([33, 132.16666666666666])
+    .bindPopup("平面直角座標3系原点")
+    .addTo(map);
+  L.marker([33, 133.5]).bindPopup("平面直角座標4系原点").addTo(map);
+  L.marker([33, 134.33333333333334])
+    .bindPopup("平面直角座標5系原点")
+    .addTo(map);
+  L.marker([33, 136.0]).bindPopup("平面直角座標6系原点").addTo(map);
+  L.marker([33, 137.16666666666666])
+    .bindPopup("平面直角座標7系原点")
+    .addTo(map);
+  L.marker([33, 138.5]).bindPopup("平面直角座標8系原点").addTo(map);
+  L.marker([33, 139.33333333333334])
+    .bindPopup("平面直角座標9系原点")
+    .addTo(map);
+  L.marker([33, 140.66666666666666]).addTo(map);
 };
