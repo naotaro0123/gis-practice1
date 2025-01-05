f2a8f597976ffb4bec8ceddfea2bdfc7ad49d9e5
naotaro0123 <n.ikewada@gmail.com>
2025-01-05 15:45:50 +0900

    fix: reference json

--- src/maplibre-map.ts
+++ src/maplibre-map.ts
@@ -11,23 +11,46 @@ export const setupMapLibreGl = (container: HTMLElement) => {
 
   map.on("load", async () => {
     const iconImage = await map.loadImage("./img/icon.png");
-    map.addImage("facility_icon", iconImage.data);
-    const jsonFileIds = ["P12a-14_14", "P12c-14_14"] as const;
-    for (const fileId of jsonFileIds) {
-      map.addSource(fileId, {
-        type: "geojson",
-        data: `./json/${fileId}.geojson`,
-      });
-      map.addLayer({
-        id: fileId,
-        type: "symbol",
-        source: fileId,
-        layout: {
-          "icon-image": "facility_icon",
-          "icon-size": 0.1,
-        },
-      });
-    }
+    const iconImageId = "facility_icon" as const;
+    map.addImage(iconImageId, iconImage.data);
+    // 国土数値情報データの刊行資源データ（神奈川）
+    // https://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-P12-2014.html
+    const fileId = "P12a-14_14";
+
+    map.addSource(fileId, {
+      type: "geojson",
+      data: `./json/${fileId}.geojson`,
+    });
+    map.addLayer({
+      id: fileId,
+      type: "symbol",
+      source: fileId,
+      layout: {
+        "icon-image": iconImageId,
+        "icon-size": 0.1,
+      },
+    });
+
+    map.on("click", fileId, (e) => {
+      const geometry = e.features?.[0].geometry;
+      const name = e.features?.[0].properties.P12_002;
+
+      if (geometry !== undefined && "coordinates" in geometry) {
+        const coordinates = (geometry.coordinates as [number, number]).slice();
+
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        new maplibregl.Popup({
+          offset: 10,
+          closeButton: false,
+        })
+          .setLngLat(coordinates as [number, number])
+          .setHTML(name)
+          .addTo(map);
+      }
+    });
   });
 
   const popup = new maplibregl.Popup({
